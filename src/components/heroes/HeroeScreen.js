492cf8e96301b7f9a65c1949f4b63091b3dbd8d5
Alessandra M <jeni73255@gmail.com>
2020-12-06 20:24:29 -0500

    ..

--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -15,7 +15,7 @@ export const HeroeScreen = ({ history }) => {
     //console.log( heroeId )
 
     if (!hero) {
-        return <Redirect to = "/" / >
+        return <Redirect to="/" />
     }
 
     const {
@@ -45,51 +45,38 @@ export const HeroeScreen = ({ history }) => {
 
 
 
-    return ( <
-        div className = "row mt-5" >
-        <
-        div className = "col-4" >
-        <
-        img src = { `../assets/heroes/${ heroeId }.jpg` }
-        alt = { superhero }
-        className = "img-thumbnail  animate__animated animate__fadeInLeft" / >
+    return (
 
-        <
-        /div>
+        <div className="row mt-5" >
+            <div className="col-4" >
+                <img src={`../assets/heroes/${ heroeId }.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail  animate__animated animate__fadeInLeft" />
 
-        <
-        div className = "col-8" >
-        <
-        h3 > { superhero } < /h3> <
-        ul className = "list-group list-group-flush" >
-        <
-        li className = "list-group-item list-group-item-info" > < b > Alter ego: < /b> { alter_ego }</li >
-        <
-        li className = "list-group-item list-group-item-info" > < b > Publisher: < /b> { publisher }</li >
-        <
-        li className = "list-group-item list-group-item-info" > < b > first_appearance: < /b> { first_appearance }</li >
-        <
-        li className = "list-group-item list-group-item-info" > < b > characters: < /b> {  characters }</li >
+            </div>
+
+            < div className="col-8" >
+                <h3 > {superhero} </h3>
+        <ul className="list-group list-group-flush" >
+                        <li className="list-group-item list-group-item-info" > <b> Alter ego: </b> {alter_ego}</li >
+                            <li className="list-group-item list-group-item-info" > <b> Publisher: </b> {publisher}</li >
+                                <li className="list-group-item list-group-item-info" > <b> first_appearance: </b> {first_appearance}</li >
+                                    < li className="list-group-item list-group-item-info" > <b> characters: </b> {characters}</li >
 
 
-        <
-        /ul>
+        </ul>
 
 
-        <
-        button className = "btn btn-secondary mt-2"
-        onClick = { handleReturn } >
-        Return <
-        /button>
+        <button className="btn btn-secondary mt-2"
+                onClick={handleReturn} >
+        Return </button>
 
 
-        <
-        /div>
+        </div>
 
 
 
 
-        <
-        /div>
+        </div>
     )
 }
