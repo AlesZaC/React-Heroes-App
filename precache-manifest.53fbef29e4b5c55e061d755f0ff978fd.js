1a2206f647a0792be8719f810ca005be3361092a
Alessandra M <jeni73255@gmail.com>
2020-12-06 19:24:36 -0500

    Updates

---
+++ precache-manifest.53fbef29e4b5c55e061d755f0ff978fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479bad2219bd5dfdea5c35d486e579ac",
+    "url": "/React-Heroes-App/index.html"
+  },
+  {
+    "revision": "4346d38a7ae1f8d1e4d0",
+    "url": "/React-Heroes-App/static/css/main.edc07bfa.chunk.css"
+  },
+  {
+    "revision": "2751bc37975a619985e2",
+    "url": "/React-Heroes-App/static/js/2.5feab702.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/React-Heroes-App/static/js/2.5feab702.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4346d38a7ae1f8d1e4d0",
+    "url": "/React-Heroes-App/static/js/main.899477c4.chunk.js"
+  },
+  {
+    "revision": "727ac0c3c1ffe95dedb4",
+    "url": "/React-Heroes-App/static/js/runtime-main.0d7fa7c6.js"
+  }
+]);
